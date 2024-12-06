9c6be0de07b4f0980fa300b9b7395c593e5a0adc
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-12-06 13:49:56 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-sha256(((a)=>a?a:'')(getCookie('key'))).then(res=>{if(res!=='f6a5a99f56ff082b24369731f69fbb764ca33d2ef817a534cf6ca4d2d5dd1a7d'){document.documentElement.innerHTML='';window.location=''}})
+sha256(((a)=>a?a:'')(getCookie('key'))).then(res => {
+    if (res !== 'f6a5a99f56ff082b24369731f69fbb764ca33d2ef817a534cf6ca4d2d5dd1a7d') {
+        document.documentElement.innerHTML='';
+        window.location = '';
+    }
+})
 
-const sha256 = async (message) => Array.from(new Uint8Array(await crypto.subtle.digest('SHA-256', new TextEncoder().encode(message)))).map(b => b.toString(16).padStart(2, '0')).join('');
+async function sha256(message) {
+    const msgBuffer = new TextEncoder().encode(message);                    
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
+}
 
 function getCookie(cookiename) {
     var cookiestring=RegExp(cookiename+"=[^;]+").exec(document.cookie);
