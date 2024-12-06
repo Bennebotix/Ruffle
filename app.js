9d911d677829ac84aeaec7af3b2f906be9a7d703
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-12-06 14:30:48 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-sha256(((a)=>a?a:'')(getCookie('key'))).then(res => {
+s(((a)=>a?a:'')(getCookie('key'))).then(res => {
     if (res !== 'f6a5a99f56ff082b24369731f69fbb764ca33d2ef817a534cf6ca4d2d5dd1a7d') {
         window.stop();
         document.documentElement.innerHTML = '';
@@ -10,15 +10,4 @@ sha256(((a)=>a?a:'')(getCookie('key'))).then(res => {
     }
 })
 
-async function sha256(message) {
-    const msgBuffer = new TextEncoder().encode(message);                    
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-    return hashHex;
-}
-
-function getCookie(cookiename) {
-    var cookiestring=RegExp(cookiename+"=[^;]+").exec(document.cookie);
-    return decodeURIComponent(!!cookiestring ? cookiestring.toString().replace(/^[^=]+./,"") : "");
-}
+const s=async e=>Array.from(new Uint8Array(await crypto.subtle.digest("SHA-256",new TextEncoder().encode(e)))).map(e=>e.toString(16).padStart(2,"0")).join(""),g=e=>{var t;return t=RegExp(e+"=[^;]+").exec(document.cookie),decodeURIComponent(t?t.toString().replace(/^[^=]+./,""):"")};
