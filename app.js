201e372050540d61ae72426c57b846e1b3595742
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-12-06 15:00:48 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-sha256(((a)=>a?a:'')(getCookie('key'))).then(res => {
-    if (res !== 'f6a5a99f56ff082b24369731f69fbb764ca33d2ef817a534cf6ca4d2d5dd1a7d') {
+crypto.subtle.digest('SHA-256', new TextEncoder().encode(((a)=>a?a:'')(getCookie('key')))).then(r=>{
+    if (Array.from(new Uint8Array(r)).map(b => b.toString(16).padStart(2, '0')).join('') !== 'f6a5a99f56ff082b24369731f69fbb764ca33d2ef817a534cf6ca4d2d5dd1a7d') {
         window.stop();
         document.documentElement.innerHTML = '';
         document.write('<head><meta name="color-scheme" content="light dark"></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">Page inaccessible. Please contact the administrator for access.</pre></body>');
@@ -13,8 +13,6 @@ sha256(((a)=>a?a:'')(getCookie('key'))).then(res => {
 // async function sha256(message) {
 //     return Array.from(new Uint8Array(await crypto.subtle.digest('SHA-256', new TextEncoder().encode(message)))).map(b => b.toString(16).padStart(2, '0')).join('');
 // }
-
-const sha256 = async (message) => Array.from(new Uint8Array(await crypto.subtle.digest('SHA-256', new TextEncoder().encode(message))).map(b => b.toString(16).padStart(2, '0')).join('');
 
 function getCookie(cookiename) {
     var cookiestring=RegExp(cookiename+"=[^;]+").exec(document.cookie);
