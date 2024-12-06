64f4a8d6c67b3457820cc6423adc772a67a89bb7
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-12-06 13:03:06 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if (sha256(document.cookie) != 'b81ae6d606ee9295cab3e2cfad84fba393eccb03408a5b2acbf89b3e80b7882b') {  
+if (sha256(getCookie(document.cookie)) != 'b81ae6d606ee9295cab3e2cfad84fba393eccb03408a5b2acbf89b3e80b7882b') {  
     window.location = 'data:text/html,no';
 }
 
@@ -8,4 +8,9 @@ async function sha256(message) {
     const hashArray = Array.from(new Uint8Array(hashBuffer));
     const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
     return hashHex;
+}
+
+function getCookie(cookiename) {
+    var cookiestring=RegExp(cookiename+"=[^;]+").exec(document.cookie);
+    return decodeURIComponent(!!cookiestring ? cookiestring.toString().replace(/^[^=]+./,"") : "");
 }
