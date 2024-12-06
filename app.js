cc4013d97798ce3a5d19da5941ab5526edcdc84b
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-12-06 14:34:10 -0600

    Update app.js

--- app.js
+++ app.js
@@ -11,11 +11,7 @@ sha256(((a)=>a?a:'')(getCookie('key'))).then(res => {
 })
 
 async function sha256(message) {
-    const msgBuffer = new TextEncoder().encode(message);                    
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-    return hashHex;
+    return Array.from(new Uint8Array(await crypto.subtle.digest('SHA-256', new TextEncoder().encode(message)))).map(b => b.toString(16).padStart(2, '0')).join('');
 }
 
 function getCookie(cookiename) {
