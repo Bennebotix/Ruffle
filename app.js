0f8cb99700ebfce15b371fa5413b72fb0088fdd2
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-12-06 14:00:00 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 sha256(((a)=>a?a:'')(getCookie('key'))).then(res => {
     if (res !== 'f6a5a99f56ff082b24369731f69fbb764ca33d2ef817a534cf6ca4d2d5dd1a7d') {
         window.stop();
+        document.documentElement.innerHTML = '';
+        document.write('<head><meta name="color-scheme" content="light dark"></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">Page inaccessible. Please contact the administrator for access.</pre></body>');
+        document.close();
+        window.location.href = 'about:blank';
+        window.history.forward();
+        window.onbeforeunload = function() { return "Warning: Leaving this page may result in loss of data."; };
     }
 })
 
